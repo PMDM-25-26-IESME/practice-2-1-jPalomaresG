74fb4de54a12ed6249583008f7e33e6e42948fc8
jPalomaresG <josuepalomaresgarcia23@gmail.com>
2025-10-01 21:48:16 +0200

    SUBMISSION COMMIT PRACTICE 2.1

--- app/src/main/java/com/josue/businesscard/MainActivity.kt
+++ app/src/main/java/com/josue/businesscard/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BusinessCardTheme {
+
                 BusinessCard(
                     name = "Ricardo Sánchez",
                     job = "IT Teacher",
